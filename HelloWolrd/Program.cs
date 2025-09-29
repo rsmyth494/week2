4477f91f257e60c426ce008f48f7a312251640d6
rsmyth494 <smyth-r73@ulster.ac.uk>
2025-09-29 15:34:15 +0100

    Changed the test message.

--- HelloWolrd/Program.cs
+++ HelloWolrd/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to com326");
-Console.WriteLine("test");
+Console.WriteLine("test1");
